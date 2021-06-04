42ac7e9451c852bfceb6df262e6168a76fdecc54
Christian Echica <echica.christian@gmail.com>
2021-06-04 16:13:51 +0800

    Added test case

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,4 +37,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
+      @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "Hello Motherfuckers!";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
